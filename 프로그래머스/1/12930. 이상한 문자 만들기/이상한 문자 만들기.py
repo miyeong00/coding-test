decb2e36ed1289555d12935f8a8ebd055a170d72
miyeong00 <94594147+miyeong00@users.noreply.github.com>
2025-12-08 11:52:07 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.03 ms, Memory: 9.11 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -0,0 +1,16 @@
+def solution(s):
+    answer = ''
+    
+    n = 0
+    for i in range(len(s)):
+        if s[i] == ' ':
+            answer += s[i]
+            n = 0
+        else:
+            if n % 2 == 0:
+                answer += s[i].upper()
+                n += 1
+            else:
+                answer += s[i].lower()
+                n += 1
+    return answer
