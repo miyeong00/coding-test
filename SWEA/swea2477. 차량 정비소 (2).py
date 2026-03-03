39f555f13d53a1ad77b3c475bcde37e92455f024
miyeong <parkmiyoung2175@gmail.com>
2026-03-03 09:48:15 +0900

    swea2477. 차량 정비소 (2).py

--- SWEA/swea2477. 차량 정비소 (2).py
+++ SWEA/swea2477. 차량 정비소 (2).py
@@ -12,6 +12,9 @@
 # 지갑을 분실한 고객과 같은 접수 창구와 같은 정비 창구를 이용한 고객의 고객번호들을 찾아 그 합을 출력
 # 만약, 그런 고객이 없는 경우 -1 출력
 
+from collections import deque
+import heapq
+
 T = int(input()) # 테스트 케이스 개수
 for test_case in range(1, T+1):
     # N : 접수 창구 개수, M : 정비 창구 개수, K : 고객 수, A : 지갑을 두고 간 고객이 이용한 접수 창구번호, B : 정비 창구번호
@@ -20,4 +23,11 @@ for test_case in range(1, T+1):
     repair_time = list(map(int, input().split())) # 정비 창구에서 정비하는 데 걸리는 시간
     visit_time = list(map(int, input().split())) # k번째 고객이 차량 정비소를 방문하는 시간
 
-    
+    wait_receipt = deque() # 접수 대기 큐, 고객 id 저장
+    wait_repair = [] # 정비 대기 큐, (접수 완료 시각, 접수창구번호, 고객 id)
+
+    reception = [None] * N # 접수 창구 상태 배열, (누가 쓰는지, 남은 시간)
+    repair = [None] * M # 정비 창구 상태 배열, (누가 쓰는지, 남은 시간)
+
+    used_A = [0] * (K + 1) # 어떤 고객이 몇 번 접수 창구를 이용했는가 => A랑 비교
+    used_B = [0] * (K + 1) # 어떤 고객이 몇 번 정비 창구를 이용했는가 => B랑 비교
