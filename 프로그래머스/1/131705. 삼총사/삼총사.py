6169a4e3062ca82a2023d9e826ae853c8da85703
miyeong01 <94594147+miyeong01@users.noreply.github.com>
2026-07-06 15:49:31 +0900

    [level 1] Title: 삼총사, Time: 0.07 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,21 +1,33 @@
+# 학생 3명의 정수 번호를 더했을 때 0이 되면 3명의 학생은 삼총사라고 함.
+# number : 학생들의 번호를 나타내는 정수 배열
+# 학생들 중 삼총사를 만들 수 있는 방법의 수 출력
+
 def solution(number):
-    answer = 0
-    n = len(number)
+    answer = 0 # 삼총사를 만들 수 있는 방법의 수
     
-    def dfs(start, count, cur_sum):
+    # 삼총사 만들기
+    # idx : 지금 몇 번째 학생을 보고 있는지
+    # count : 지금까지 몇 명을 뽑았는지
+    # total : 뽑은 학생 번호의 합 
+    def triple(idx, count, total):
         nonlocal answer
-        
-        # 종료조건 : 3명 다 뽑았을 때
+
+        # 학생 3명을 모두 뽑은 경우
         if count == 3:
-            if cur_sum == 0:
+            if total == 0:
                 answer += 1
             return
+
+        # 더 볼 학생이 없으면 종료 
+        if idx == len(number):
+            return
         
-        # 탐색
-        for i in range(start, n):
-            # 다음 재귀 호출
-            dfs(i+1, count+1, cur_sum+number[i])
+        # 1. 현재 학생을 뽑는 경우
+        triple(idx + 1, count + 1, total + number[idx])
+        
+        # 2. 현재 학생을 뽑지 않는 경우
+        triple(idx + 1, count, total)
     
-    dfs(0,0,0)
+    triple(0, 0, 0)
     
     return answer
