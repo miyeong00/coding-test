eeee8c920b784cd1803bdf3350d682a0fc1776d3
miyeong01 <94594147+miyeong01@users.noreply.github.com>
2026-08-12 10:15:59 +0900

    [level 1] Title: 햄버거 만들기, Time: 113.31 ms, Memory: 28.2 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -1,14 +1,18 @@
+# 빵(1) - 야채(2) - 고기(3) - 빵(1) 
+# ingredient : 상수에게 전해지는 재료의 정보를 나타내는 정수 배열
+# 상수가 포장하는 햄버거의 개수 출력
+
 def solution(ingredient):
-    answer = 0 # 상수가 포장하는 햄버거의 개수
-    stack = []
+    answer = 0 # 상수가 포장하는 햄버거의 개수 
+    stack = [] # 햄버거 재료 쌓는 스택
     
     for i in ingredient:
         stack.append(i)
         
         if len(stack) >= 4:
             if stack[-4:] == [1, 2, 3, 1]:
-                answer += 1
-                for _ in range(4):
+                for i in range(4):
                     stack.pop()
-    
+                answer += 1
+                
     return answer
